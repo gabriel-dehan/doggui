2007265fd72fc5b6e33b9a4d895a0fe952e552e8
Yann Leost <yannleost@MacBook-Pro-de-Yann.local>
2018-07-12 16:01:36 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :user
   #####################
   #####################
   get 'legislation', to: 'pages#', as: :legislation
   get 'contact', to: 'pages#', as: :contact
   get 'qui-sommes-nous', to: 'pages#', as: :quisommesnous
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get 'dogs',     to: 'dogs#index', as: :index
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "seeding one user"
+10.times do 
+  user = User.create!(
+	email: Faker::Internet.email,
+	password: "azerty",
+	)
+end
+
+p "user created, let's seed dogs"
+
+
+10.times do
+  dog = Dog.create!(
+    nickname: Faker::Coffee.variety,
+    size: Faker::Number.between(10, 150),
+    breed: "brown",
+    user: User.all.sample
+    )
+ end
+
+ p "dog & user seeded"
