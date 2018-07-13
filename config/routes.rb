c03a34ffb7e1eb83714edbcf576e6ba6b036c4b2
Yann Leost <yannleost@MacBook-Pro-de-Yann.local>
2018-07-13 17:36:33 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#', as: :contact
   get 'qui-sommes-nous', to: 'pages#', as: :quisommesnous
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'dogs',     to: 'dogs#index', as: :index
+ resources :dogs
 
 end
