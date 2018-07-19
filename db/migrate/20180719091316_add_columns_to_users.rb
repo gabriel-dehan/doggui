8a0bf228fa821e3b55e92a83cbc6ad218ed1d3d7
Yann Leost <yannleost@MacBook-Pro-de-Yann.local>
2018-07-19 11:41:03 +0200

    ok

---
+++ db/migrate/20180719091316_add_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :first_name, :string
+  	add_column :users, :last_name, :string
+  	add_column :users, :avatar, :string
+  	add_column :users, :siret, :string
+  	add_column :users, :address, :string
+  	add_column :users, :telephone, :string
+  end
+end
