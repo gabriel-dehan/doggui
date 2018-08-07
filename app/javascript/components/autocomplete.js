9941bc274b1a36d1196f042a91598b60534af7fc
guillaume aa <guillaume.aa@gmail.com>
2018-08-07 10:19:40 +0200

    ok few details

---
+++ app/javascript/components/autocomplete.js
@@ -0,0 +1,16 @@
+function autocomplete() {
+  document.addEventListener("DOMContentLoaded", function() {
+    var dogAddress = document.getElementById('dog_address');
+
+    if (dogAddress) {
+      var autocomplete = new google.maps.places.Autocomplete(dogAddress, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(dogAddress, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
+}
+
+export { autocomplete };
