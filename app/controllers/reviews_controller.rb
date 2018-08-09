05c047554c6b95a2d91ab861dc9bf10f7d6c802c
Yann Leost <yannleost@MacBook-Pro-de-Yann.local>
2018-08-09 17:06:29 +0200

    ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,16 +2,14 @@ class ReviewsController < ApplicationController
   def new
   	@review = Review.new
     authorize @review
-    @dog = Dog.find(params[:dog_id])
-    @booking = Booking.find(params[:booking_id])
   end
 
   def create
   	@review = Review.new(review_params)
     authorize @review
-    @dog = @review.booking.dog 
-    @booking = @review.booking
-    @review.save!
+    @dog
+    @dog
+    @review.save
   	if @review.save
      redirect_to dogs_path(@review)
    else
