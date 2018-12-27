58a9ad6e86c6b246d5e572141098b60397d79fba
thibault <thibault.finot@gmail.com>
2018-12-27 18:10:11 +0100

    add dependent destroy to dog

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ApplicationRecord
   belongs_to :dog
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   scope :of_dog, ->(dog) { where(dog: dog) }
   scope :of_sender, ->(user) { joins(:messages).where(conversation_messages: { user_id: user }) }
