81379b5908ef5e8dc3087df52b0cbad86a09c627
thibault <thibault.finot@gmail.com>
2019-01-07 16:18:40 +0100

    vote for dogs work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
-  
+
   get 'contact_form/new'
   get 'contact_form/create'
   devise_for :users, controllers: { registrations: 'registrations' }
