297781ec509a07a243613cf0d6b7e40d1e190d8a
thibault <thibault.finot@gmail.com>
2019-01-15 16:36:14 +0100

    add config of heroku action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.web_socket_server_url = "wss://molosses.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://molosses.herokuapp.com', 'http://molosses.herokuapp.com']
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
