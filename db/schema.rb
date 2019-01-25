9d280479b6ec5f39f2381bff549e1768e60ec386
Gabriel <dehan.gabriel@gmail.com>
2019-01-25 20:42:51 +0100

    Removed DS_Store, Fixed seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_01_11_145019) do
 ActiveRecord::Schema.define(version: 2019_01_15_130011) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,12 +30,12 @@ ActiveRecord::Schema.define(version: 2019_01_15_130011) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "breeds", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
 
   create_table "contacts", force: :cascade do |t|
