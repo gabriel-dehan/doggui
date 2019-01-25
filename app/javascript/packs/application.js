5fb2b09b372cb504638ab8b81a6d77d24ae9b469
guillaume AA <guillaume.aa@gmail.com>
2019-01-25 17:05:46 +0100

    card details

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initMapbox();
 initAutocomplete();
+
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
