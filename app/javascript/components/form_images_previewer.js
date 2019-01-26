755f77c4622cf326bab5f7e7b9b467d1df7d46e8
Gabriel <dehan.gabriel@gmail.com>
2019-01-26 04:26:59 +0100

    Image previews and upload button

--- app/javascript/components/form_images_previewer.js
+++ app/javascript/components/form_images_previewer.js
@@ -1,23 +1,60 @@
-
 const FormImagesPreviewer = () => {
+  // We replaced the basic html inputs with custom buttons
+  activateCustomUploadButtons();
+
   // For each file input with the class `preview`
   document.querySelectorAll('input[type=file].preview').forEach((input) => {
     EnablePreview(input);
+    updateUploadButtonText(input);
   })
+
+  // When an image is chosen, update the upload's button text
+  function updateUploadButtonText(input) {
+    input.addEventListener('change', (e) => {
+      var filesCount = input.files.length;
+      var button = input.parentNode.parentNode.querySelector('.upload-button');
+
+      if (filesCount > 1) {
+        button.innerHTML = "Changer les images";
+      } else if (filesCount > 0) {
+        button.innerHTML = "Changer l'image";
+      } else { 
+        button.innerHTML = "Ajouter une image";
+      }
+    });
+
+  }
+
+  function activateCustomUploadButtons() {
+    document.querySelectorAll('.upload-input-container .upload-button').forEach((button) => {
+      // Whenever you click on an upload button it clicks on the hidden file input next to it
+      button.addEventListener('click', function(e) {
+        var input = e.target.parentNode.querySelector('input[type=file]');
+        input.click();
+      });
+    });
+  }
 
   function EnablePreview(input) {
     input.addEventListener('change', (e) => {
+      // Handle preview
       if (input.files && input.files.length > 0) {
-        Array.prototype.forEach.call(input.files, (file) => {
+        let previewContainer = input.parentNode.parentNode.querySelector('.preview-container');
+
+        // Empty the preview container
+        previewContainer.innerHTML = '';
+
+        // For each file, display a preview in the container
+        Array.prototype.forEach.call(input.files, (file, index) => {
           var reader = new FileReader();
   
           reader.onload = function (e) {
-              // Twice parent nodes because simple form creates a <div class="form-group"> around the input
-              let previewContainer = input.parentNode.parentNode.querySelector('.preview-container');
               let pictureElement = document.createElement("img");
 
-              console.log(input.parentNode, previewContainer);
               pictureElement.setAttribute('src', e.target.result);
+              //pictureElement.addEventListener('click', removeFiles);
+
+              // Add the preview picture to the container
               previewContainer.appendChild(pictureElement);
            };
   
@@ -26,7 +63,16 @@ const FormImagesPreviewer = () => {
       }
     });
   }
+
+  function removeFiles(e) { 
+    e.target.parentNode.parentNode.querySelector('input[type=file].preview').value = "";
+    e.target.parentNode.innerHTML = "";
+  }
 };
+
+
+
+
 
 
 export { FormImagesPreviewer };
