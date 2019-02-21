90e6e880e003e816131187acea570e73e79d4c93
thibault <thibault.finot@gmail.com>
2019-02-21 10:27:49 +0100

    to debug the name in conversation add the first_name in user registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  
+
   protected
 
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
 
-
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :dogs
   has_many :messages, class_name: 'Conversation::Message'
 
+  validates :first_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
