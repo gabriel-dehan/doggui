21a0622505a2f4baac0f24aaef52ba5bba1e18ab
guillaume AA <guillaume.aa@gmail.com>
2019-03-29 16:31:25 +0100

    meta title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
 end
+
+
+def title(title_content)
+  if title_content.present? # si le paramètre title_content est présent
+    title_content << " | Doggui" # le title reprendra la chaîne en paramètre suivie de " | Mon Site"
+  else
+    "Doggui : spécialisé pour la saillie canine - Trouver un étalon mâle LOF" # sinon, par défaut, le title sera "Mon site"
+  end
+end
